4df00fc22956b39fd81e3fd4f26985056b45cdac
rubaiyat12 <rubaiyatraha12@gmail.com>
2023-12-16 23:03:02 -0800

    Problem-1 part

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [tasks, setTasks] = useState([]);
 
-    const handleClick = (val) =>{
+  
+
+    const handleClick = (val) => {
         setShow(val);
     }
+    const handleSubmitForm = (event) => {
+        event.preventDefault();
+        const name = event.target.elements.name.value;
+        const status = event.target.elements.status.value;
+        const newTask = { name, status };
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+        event.target.elements.name.value = '';
+        event.target.elements.status.value = '';
+    };
+
+    const getSortedTasks = () => {
+        // Sort all tasks by All, Active, Completed order
+        const sortedTasks = tasks.sort((a, b) => {
+          const order = [ 'Completed','Active'];
+          if (order.indexOf(a.status) === order.indexOf(b.status)) {
+            return 0;
+          }
+          return order.indexOf(a.status) > order.indexOf(b.status) ? -1 : 1;
+        });
+    
+        // Filter tasks
+        return show === 'all' ? sortedTasks : sortedTasks.filter((task) => task.status.toLowerCase() === show);
+      };
+
+    const sortedTasks = getSortedTasks();
 
     return (
 
@@ -14,40 +42,69 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form
+                        className="row gy-2 gx-3 align-items-center mb-4"
+                        onSubmit={handleSubmitForm}
+                    >
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" required className="form-control" placeholder="Name" name="name" />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" required className="form-control" placeholder="Status" name="status" />
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button
+                                className={`nav-link ${show === 'all' && 'active'}`}
+                                type="button"
+                                onClick={() => handleClick('all')}
+                            >
+                                All
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button
+                                className={`nav-link ${show === 'active' && 'active'}`}
+                                type="button"
+                                onClick={() => handleClick('active')}
+                            >
+                                Active
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button
+                                className={`nav-link ${show === 'completed' && 'active'}`}
+                                type="button"
+                                onClick={() => handleClick('completed')}
+                            >
+                                Completed
+                            </button>
                         </li>
                     </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
+                    <table className="table table-striped">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {
+                                sortedTasks.map((task, index) => (
+                                    <tr key={index}>
+                                        <td>{task.name}</td>
+                                        <td>{task.status}</td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
                     </table>
                 </div>
